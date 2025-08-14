e22bc7c96fc65f8c91bfdaca3b255852226c9089
Shorena Nemsadze <shorenanemsadze96@gmail.com>
2025-08-13 21:23:53 -0700

    Initial commit

---
+++ quizz.py
@@ -0,0 +1,17 @@
+import random
+print("Welcome to the Simple Multiplication Quizz!")
+
+i = 1
+while i <= 5:
+    num_1 = random.randint(1, 10)
+    num_2 = random.randint(1, 10)
+    answer = num_1 * num_2
+
+    user_input = int(input(f"Problem {i}: What is {num_1} x {num_2}? "))
+
+    if user_input == answer:
+        print("Correct!")
+    else:
+        print(f"Incorrect! The correct answer is {answer}")
+
+    i += 1
